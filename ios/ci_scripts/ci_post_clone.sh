89f1cb0ca7ee238555d775eb965ac36dbe11c861
Marvin <Marvin.Jaeckisch1@web.de>
2023-11-23 20:11:24 +0100

    :sparkles: update dependencies

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-# The default execution directory of this script is the ci_scripts directory.
-cd $CI_WORKSPACE # change working directory to the root of your cloned repo.
+# Fail this script if any subcommand fails.
+set -e
 
-# Install Flutter using git.
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+# The default execution directory of this script is the ci_scripts directory.
+cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
+
+# Specify the Flutter tag you want to checkout.
+FLUTTER_TAG="3.13.6"
+
+# Install Flutter using git with the specified tag.
+git clone https://github.com/flutter/flutter.git --depth 1 --branch $FLUTTER_TAG $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
+
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
 flutter precache --ios
