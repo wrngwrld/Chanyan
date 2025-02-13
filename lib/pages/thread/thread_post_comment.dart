cfda4b7acb804b6b33d80eaf1a8605115a9952f0
wrngwrld <Marvin.Jaeckisch1@web.de>
2025-02-13 22:15:43 +0100

    :sparkles: Add quotes, spoiler an link to comments

--- lib/pages/thread/thread_post_comment.dart
+++ lib/pages/thread/thread_post_comment.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chan/Models/post.dart';
 import 'package:flutter_chan/blocs/theme.dart';
 import 'package:flutter_chan/pages/thread/thread_replied_to.dart';
 import 'package:flutter_chan/services/string.dart';
+import 'package:flutter_chan/widgets/spoiler_text.dart';
 import 'package:html/parser.dart' show parse;
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ThreadPostComment extends StatelessWidget {
   const ThreadPostComment({
@@ -26,6 +29,9 @@ class ThreadPostComment extends StatelessWidget {
     ThemeChanger theme,
     BuildContext context,
   ) {
+    final theme = Provider.of<ThemeChanger>(context);
+    final isDark = theme.getTheme() == ThemeData.dark();
+
     final List<Widget> formattedCom = [];
 
     final List<String> splittedCom = com.split('<br>');
@@ -59,17 +65,153 @@ class ThreadPostComment extends StatelessWidget {
             ),
           ),
         );
-      } else {
+      } else if (line.contains('<span class="quote">')) {
+        final text = unescape(cleanTags(line));
         formattedCom.add(
           SelectableText(
-            unescape(cleanTags(line)),
-            style: TextStyle(
-              color: theme.getTheme() == ThemeData.dark()
-                  ? Colors.white
-                  : Colors.black,
+            text,
+            style: const TextStyle(
+              color: Color(0xFF789922),
+              fontFamily: 'Arial',
             ),
           ),
         );
+      } else if (line.contains('<s>')) {
+        final List<InlineSpan> spans = [];
+        final RegExp spoilerPattern = RegExp(r'<s>(.*?)</s>');
+        final String remainingText = line;
+        int lastEnd = 0;
+
+        for (final match in spoilerPattern.allMatches(line)) {
+          // Add non-spoiler text before the match
+          if (match.start > lastEnd) {
+            final normalText = unescape(
+                cleanTags(remainingText.substring(lastEnd, match.start)));
+            spans.add(
+              TextSpan(
+                text: normalText,
+                style: TextStyle(
+                  color: theme.getTheme() == ThemeData.dark()
+                      ? Colors.white
+                      : Colors.black,
+                ),
+              ),
+            );
+          }
+
+          // Add spoiler text
+          final spoilerText = unescape(cleanTags(match.group(1) ?? ''));
+          spans.add(
+            WidgetSpan(
+              child: SpoilerText(text: spoilerText),
+            ),
+          );
+
+          lastEnd = match.end;
+        }
+
+        // Add remaining non-spoiler text after the last match
+        if (lastEnd < line.length) {
+          final normalText =
+              unescape(cleanTags(remainingText.substring(lastEnd)));
+          spans.add(
+            TextSpan(
+              text: normalText,
+              style: TextStyle(
+                color: theme.getTheme() == ThemeData.dark()
+                    ? Colors.white
+                    : Colors.black,
+              ),
+            ),
+          );
+        }
+
+        formattedCom.add(
+          SelectableText.rich(
+            TextSpan(children: spans),
+            style: const TextStyle(fontFamily: 'Arial'),
+          ),
+        );
+      } else if (line.isEmpty) {
+        formattedCom.add(const SizedBox(height: 8));
+      } else {
+        final RegExp urlPattern = RegExp(
+          r'https?://[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?',
+          caseSensitive: false,
+        );
+
+        final text = unescape(cleanTags(line));
+        final matches = urlPattern.allMatches(text);
+
+        if (matches.isEmpty) {
+          formattedCom.add(
+            SelectableText(
+              text,
+              style: TextStyle(
+                color: theme.getTheme() == ThemeData.dark()
+                    ? Colors.white
+                    : Colors.black,
+                fontFamily: 'Arial',
+              ),
+            ),
+          );
+        } else {
+          final List<TextSpan> spans = [];
+          int lastEnd = 0;
+
+          for (final match in matches) {
+            if (match.start > lastEnd) {
+              spans.add(
+                TextSpan(
+                  text: text.substring(lastEnd, match.start),
+                  style: TextStyle(
+                    color: theme.getTheme() == ThemeData.dark()
+                        ? Colors.white
+                        : Colors.black,
+                  ),
+                ),
+              );
+            }
+
+            spans.add(
+              TextSpan(
+                text: text.substring(match.start, match.end),
+                style: TextStyle(
+                  color: isDark
+                      ? const Color(0xFF81a2be)
+                      : const Color(0xFF1d9bf0),
+                ),
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () {
+                    final url = text.substring(match.start, match.end);
+                    launchUrl(Uri.parse(url));
+                  },
+              ),
+            );
+
+            lastEnd = match.end;
+          }
+
+          if (lastEnd < text.length) {
+            spans.add(
+              TextSpan(
+                text: text.substring(lastEnd),
+                style: TextStyle(
+                  color: theme.getTheme() == ThemeData.dark()
+                      ? Colors.white
+                      : Colors.black,
+                ),
+              ),
+            );
+          }
+
+          formattedCom.add(
+            Text.rich(
+              TextSpan(children: spans),
+              style: const TextStyle(fontFamily: 'Arial'),
+            ),
+          );
+        }
       }
     }
 
